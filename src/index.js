489d4440f0f1aa9128bfabb4abaf0d73e1464e0c
Jason Yang <canyoncrestjy@gmail.com>
2021-07-31 14:55:00 -0700

    can add items to bin now
    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import reducer, { initialState } from "./reducer";
 import { StateProvider } from "./StateProvider";
 
@@ -21,4 +21,4 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
