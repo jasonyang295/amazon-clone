210e975d6ce95a95af6473a3f5e402133ccf5274
Jason Yang <canyoncrestjy@gmail.com>
2021-07-31 14:44:27 -0700

    updated reducer listener

--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,14 @@ export const initialState = {
 export const getBasketTotal = (basket) => 
     basket?.reduce((amount, item) => item.price + amount, 0);
     
-    
+const reducer = (state, action) => {
+     console.log(action);
+     switch (action.type) {
+         default: 
+            return state;
+     }
+};
+ 
+export default reducer;
     
     
