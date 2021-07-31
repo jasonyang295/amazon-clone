e1d189f880b63f4df434644705bb8b287cb77c35
Jason Yang <canyoncrestjy@gmail.com>
2021-07-31 14:38:32 -0700

    fixed compiling bug

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Product.css";
 
-function Product() {
+function Product({id, title, image, price, rating}) {
     return (
         <div className = "product">
             <div className = "product_info">
