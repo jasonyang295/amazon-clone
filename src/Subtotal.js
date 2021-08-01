f4bb5862ccd7efa6387473adbc18f20f4b95e781
Jason Yang <canyoncrestjy@gmail.com>
2021-08-01 11:44:48 -0700

    define func

---
+++ src/Subtotal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
+
+function Subtotal() {
+  const history = useHistory();
+  const [{ basket }, dispatch] = useStateValue();
