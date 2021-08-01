bdeb0eed4bfbfa54b6c76aad1c9a21086daa6335
Jason Yang <canyoncrestjy@gmail.com>
2021-08-01 11:41:57 -0700

    making checkout product section

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import './CheckoutProduct.css'
 import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        // remove the item from the basket
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
