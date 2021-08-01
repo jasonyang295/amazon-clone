1497a4663f32b00c66a3bbcdb5ef23e0ba7ba6aa
Jason Yang <canyoncrestjy@gmail.com>
2021-08-01 11:47:00 -0700

    initliaze func

---
+++ src/Checkout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
+function Checkout() {
+  const [{ basket, user }, dispatch] = useStateValue();
