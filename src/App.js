c510965465f3fabb642a84f9e7641d6286cb4aa6
Jason Yang <canyoncrestjy@gmail.com>
2021-08-01 11:41:39 -0700

    app is functional

--- src/App.js
+++ src/App.js
@@ -4,22 +4,53 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
 import Login from "./Login";
+import { auth } from "./firebase"
+import { useStateValue } from "./StateProvider"
+
 function App() {
-return (
-  <div className="app">
-    <Router>
-      <Switch>
-      <Route path="/login">
-        <Login />
-      </Route>
-        <Route path="/">
-        <Header/>
-        <Home />
+
+  // const [{}, dispatch] = useStateValue();
+
+  // useEffect(() => {
+  //   // will only run once when the app component loads...
+
+  //   auth.onAuthStateChanged(authUser => {
+  //     console.log("THE USER IS >>> ", authUser);
+
+  //     if (authUser) {
+  //       // the user just logged in / the user was logged in
+
+  //       dispatch({
+  //         type: "SET_USER",
+  //         user: authUser,
+  //       });
+  //     } else {
+  //       // the user is logged out
+  //       dispatch({
+  //         type: "SET_USER",
+  //         user: null,
+  //       });
+  //     }
+  //   });
+  // }, []);
+
+
+
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+        <Route path="/login">
+          <Login />
         </Route>
-      </Switch>
-    </Router>
-  </div>
-);
+          <Route path="/">
+          <Header/>
+          <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import './CheckoutProduct.css'
+import { useStateValue } from "./StateProvider";
